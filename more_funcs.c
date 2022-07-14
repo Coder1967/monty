e20a49dab64d5a50719400393b826ff38f0187c9
nosakhare <nosakhareaghedo42@gmail.com>
2022-07-14 05:16:57 +0000

    monty interpreter added

---
+++ more_funcs.c
@@ -0,0 +1,27 @@
+#include "monty.h"
+
+void swap(stack_t **stack, unsigned int l)
+{
+	stack_t *tmp = *stack;
+	int temp;
+	if (tmp == NULL || tmp->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", l);
+		exit(EXIT_FAILURE);
+	}
+	temp = tmp->n;
+	tmp->n = tmp->next->n;
+	tmp->next->n = temp;
+}
+void add(stack_t **stack, unsigned int l)
+{
+	stack_t *tmp = *stack;
+	int res;
+	if (tmp == NULL || tmp->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", l);
+		exit(EXIT_FAILURE);
+	}
+	res = tmp->n + tmp->next->n;
+	tmp->n = res;
+}
--- stack_funcs.c
+++ stack_funcs.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include "monty.h"
 
 extern int value;
 
@@ -64,31 +64,3 @@ void nop(stack_t **stack, unsigned int l)
 	return;
 }
 
-void swap(stack_t **stack, unsigned int l)
-{
-	stack_t *tmp = *stack;
-	int temp;
-
-	if (tmp == NULL || tmp->next == NULL)
-	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", l);
-		exit(EXIT_FAILURE);
-	}
-	temp = tmp->n;
-	tmp->n = tmp->next->n;
-	tmp->next->n = temp;
-}
-
-void add(stack_t **stack, unsigned int l)
-{
-	stack_t *tmp = *stack;
-	int res;
-
-	if (tmp == NULL || tmp->next == NULL)
-	{
-		fprintf(stderr, "L%u: can't add, stack too short\n", l);
-		exit(EXIT_FAILURE);
-	}
-	res = tmp->n + tmp->next->n;
-	tmp->n = res;
-}
